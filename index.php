bbe6aa2c452826c8908c58ed9d342fc33ad57dd6
pankajcentennial <pankaj@centennialinfotech.com>
2026-02-18 20:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -225,6 +225,7 @@ $jobListDropdown = $jobStmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <title>Daily Job Assign Report</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="bg-light">
