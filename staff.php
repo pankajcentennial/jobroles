fdcf5059acbf233c27a41ee566bcc0d81b11ba7b
Pankaj <pankaj@centennialinfotech.com>
2026-02-22 13:29:55 +0530

    staff phone number format

--- staff.php
+++ staff.php
@@ -70,6 +70,8 @@ if (isset($_GET['edit'])) {
 <head>
     <title>Staff Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body class="bg-light">
